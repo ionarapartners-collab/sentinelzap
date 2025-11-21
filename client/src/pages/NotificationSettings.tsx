c7ac207bf8d97a00661587d212844a2171f2cc97
ionarapartners-collab <ionara.partners@gmail.com>
2025-11-21 14:06:04 -0300

    Update NotificationSettings.tsx

--- client/src/pages/NotificationSettings.tsx
+++ client/src/pages/NotificationSettings.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "@/_core/hooks/useAuth";
+import { useAuth } from "@/useAuth";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
